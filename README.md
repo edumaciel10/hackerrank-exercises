940f69a52fefeff64336101c2798ab04db67db7e
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2022-10-12 23:41:59 -0300

    feat(solutions): add first solutions and readme.md (#2)

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hackerrank-exercises
+
+This repo contains my solutions to the exercises on [HackerRank](https://www.hackerrank.com/) using Typescript
+
+## How to use this repo to run your code locally
+
+1. Clone this repo
+2. run `yarn install` or `npm install`
+3. Put your actual exercise code on `src/main.ts` file
+4. Change `1.in` to the input of your exercise
+5. run `yarn dev` or `npm run dev`
+6. Only saves the changes and the your logs output will be displayed on the terminal
---
+++ src/plusMinus.ts
@@ -0,0 +1,63 @@
+'use strict';
+import { WriteStream, createWriteStream } from "fs";
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr: number[]): number[] {
+    const arrLength = arr.length;
+
+    const postiveNumbersLength = arr.filter((val) =>val >0).length
+
+    const negativeNumberslength = arr.filter((val) => val<0).length
+
+    const zeroNumbersLength = arr.filter((val) => val == 0).length
+
+    return [
+        postiveNumbersLength/arrLength,
+        negativeNumberslength/arrLength,
+        zeroNumbersLength/arrLength,
+    ]
+
+}
+
+function main() {
+    // const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
+    const ws: WriteStream = createWriteStream("ouput.txt");
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = plusMinus(arr);
+
+    result.map((val) => {
+        ws.write(val + '\n');
+    })
+    ws.end();
+}
